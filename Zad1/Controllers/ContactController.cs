17170f98cfd61eeac27a44acc75b432b486fb68d
Luka Perić <peric.luka9@gmail.com>
2020-12-09 18:49:40 +0100

    Gotov 3. zadatak

--- Zad1/Controllers/ContactController.cs
+++ Zad1/Controllers/ContactController.cs
@@ -23,5 +23,13 @@ namespace Zad1.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
